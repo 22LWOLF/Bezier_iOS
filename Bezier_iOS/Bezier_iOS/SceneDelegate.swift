5478f0e3ce9d7b2212eab5403d5bb72c7078ce57
22LWOLF <142831758+22LWOLF@users.noreply.github.com>
2026-02-03 15:18:23 -0600

    Got Login screen. other problems
    Have all the stuff required for a login screen but I cannot get the button to send me to a new "page" but I also have no errors. I need to figure out how to get to another screen.

--- Bezier_iOS/Bezier_iOS/SceneDelegate.swift
+++ Bezier_iOS/Bezier_iOS/SceneDelegate.swift
@@ -11,7 +11,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    // SceneDelegate.swift
+    func changeRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard let window = self.window else { return }
+        window.rootViewController = vc
+        guard animated else { return }
+        UIView.transition(with: window,
+                          duration: 0.3,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
+    }
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
